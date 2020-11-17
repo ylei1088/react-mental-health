76b7b912e73bdb14f28298a73b7d0d1ed95546f1
Tyler Morales <moratyle@gmail.com>
2020-11-16 22:06:13 -0600

    🚧 Tutorial with Mark

--- src/pages/CreateMain.js
+++ src/pages/CreateMain.js
@@ -1,22 +1,44 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
+import React, { useState } from 'react';
 
 export default function CreateMain() {
+  const [pageIndex, setPageIndex] = useState(1);
+  function handleCount() {
+    setPageIndex(pageIndex + 1);
+  }
   return (
-    <div>
-      <h1>Create Page</h1>
-      <CreateContent />
+    <div className="create__main">
+      <h1>Create Page {pageIndex}</h1>
+      <div className="">
+        <p>whats on your mind</p>
+
+        {pageIndex >= 10 ? 'greater than 10' : 'less than 10'}
+        {pageIndex <= 10 ? (
+          <FirstComponent
+            pageIndex={pageIndex}
+            name="Tyler"
+            handleCount={handleCount}
+          />
+        ) : null}
+        <form>
+          <textarea name="lantern_body" cols="30" rows="10" />
+          <button type="submit">Next</button>
+        </form>
+      </div>
     </div>
   );
 }
 
-function CreateContent() {
+function FirstComponent({ pageIndex, name, handleCount }) {
   return (
-    <div className="">
-      <p>whats on your mind</p>
-      <form>
-        <textarea name="lantern_body" cols="30" rows="10" />
-        <button type="button">Next</button>
-      </form>
+    <div>
+      <h1>First Component</h1>
+      <p>{name}</p>
+      <h3>I am the child {pageIndex}</h3>
+      <button type="button" onClick={handleCount}>
+        Count: +1
+      </button>
     </div>
   );
 }
