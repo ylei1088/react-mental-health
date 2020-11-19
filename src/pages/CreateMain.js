7160d831c8cbd2ac9cff09f62ea49774998cb780
Tyler Morales <moratyle@gmail.com>
2020-11-18 20:26:32 -0600

    ♻️  Refactor out form and add dynamic btn to change component

--- src/pages/CreateMain.js
+++ src/pages/CreateMain.js
@@ -3,49 +3,46 @@
 import React, { useState } from 'react';
 
 export default function CreateMain() {
-  const [pageIndex, setPageIndex] = useState(1);
-  function handleCount() {
-    setPageIndex(pageIndex + 1);
+  const [btnIndex, setBtnIndex] = useState(1);
+  function handleCount(e) {
+    setBtnIndex(btnIndex + 1);
+    e.preventDefault();
   }
   return (
     <div className="create__main">
-      <h1>What's on your mind?</h1>
-
-      <form className="create__main__form">
-        <textarea
-          name="lantern_body"
-          cols="30"
-          rows="10"
-          placeholder="How's your day going?"
-        />
-        <button className="btn--next" type="submit">
-          Next
-        </button>
-      </form>
-
-      <h6 className="no-show">
-        {pageIndex >= 10 ? 'greater than 10' : 'less than 10'}
-        {pageIndex <= 10 ? (
-          <FirstComponent
-            pageIndex={pageIndex}
-            name="Tyler"
-            handleCount={handleCount}
-          />
-        ) : null}
-      </h6>
+      {btnIndex === 1 ? (
+        <CreateLanternForm btnIndex={btnIndex} handleCount={handleCount} />
+      ) : null}
+      {btnIndex === 2 ? (
+        <ChooseLanternColor btnIndex={btnIndex} handleCount={handleCount} />
+      ) : null}
+      <button className="btn--next" type="submit" onClick={handleCount}>
+        Next
+      </button>
     </div>
   );
 }
 
-function FirstComponent({ pageIndex, name, handleCount }) {
+function CreateLanternForm() {
   return (
     <div>
-      <h1>First Component</h1>
-      <p>{name}</p>
-      <h3>I am the child {pageIndex}</h3>
-      <button type="button" onClick={handleCount}>
-        Count: +1
-      </button>
+      <h1>What's on your mind?</h1>
+
+      <form className="create__main__form">
+        <textarea name="lantern_body" placeholder="How's your day going?" />
+      </form>
+    </div>
+  );
+}
+
+function ChooseLanternColor() {
+  return (
+    <div>
+      <h1>Choose a Color</h1>
+
+      <form className="create__main__form">
+        <textarea name="lantern_body" placeholder="How's your day going?" />
+      </form>
     </div>
   );
 }
